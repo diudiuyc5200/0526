d4b43b24df5c18cc4165eff5ab0ea67a3a64c236
diudiuyc5200 <17766728994@163.com>
2026-05-26 21:11:24 +0800

    Update vmscan.c

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -177,9 +177,9 @@ unsigned long sysctl_clean_low_kbytes __read_mostly = CONFIG_CLEAN_LOW_KBYTES;
 unsigned long sysctl_clean_min_kbytes __read_mostly = CONFIG_CLEAN_MIN_KBYTES;
 
 /*
- * From 0 .. 100.  Higher means more swappy.
+ * From 0 .. 200.  Higher means more swappy.
  */
-int vm_swappiness = 60;
+int vm_swappiness = 150;
 /*
  * The total number of pages which are beyond the high watermark within all
  * zones.
